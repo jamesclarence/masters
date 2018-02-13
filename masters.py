347f517ac7e09723642a1991febf8fd0f33c4e32
Jay Fisher <fisher.jc@gmail.com>
2018-02-12 22:33:31 -0500

    edit year range bc of wwii; start function to read url list

--- masters.py
+++ masters.py
@@ -6,18 +6,35 @@ import pandas as pd
 # Range of years (1934-2017)
 year_list = list(range(1934, 2018))
 
-# Get list of urls
-'http://www.augusta.com/masters/historic/leaderboards/' + year + 'leaderboard.shtml'
+# Remove years 1943-45 because of World War II
+del year_list[9:12]
 
-
+# Get list of urls with Masters results 
+url_list = []
 for i in year_list:
-	l = 'http://www.augusta.com/masters/historic/leaderboards/' + str(i) + 'leaderboard.shtml'
-	print(l)
+	u = 'http://www.augusta.com/masters/historic/leaderboards/' + str(i) + 'leaderboard.shtml'
+	url_list.append(u)
+
+url_dict = dict(year = year_list, url = url_list)
+
+# Read tables from url, assign year, and add to leaderboard variable
+leaderboard = []
+for year, url in url_dict.items(): 
+	t = pd.read_html(url, header=0)
+	t_df = t[0]
+	t_df.assign(year = str(year))
+	leaderboard.append(t_df)
+
+# for u in url_list: 
+# 	t = pd.read_html(u, header=0)
+# 	t_df = t[0]
+# 	leaderboard.append(t_df)	
 
 # specify the url
 url = 'http://www.augusta.com/masters/historic/leaderboards/1969leaderboard.shtml'
 
-# Use pandas (separate from bs4)
+# Read table from url
 table = pd.read_html(url,  header=0)
 table_df = table[0]
 table_df.assign(year = '1969')
+
